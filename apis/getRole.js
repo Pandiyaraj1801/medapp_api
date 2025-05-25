b1b2823f6024a47f0aae5d5144266a52eef4c9ae
Pandiyaraj1801 <pandiyaraj1801@gmail.com>
2025-05-25 12:30:59 +0530

    add the cors policy statement to allow the server connection

--- apis/getRole.js
+++ apis/getRole.js
@@ -1,6 +1,11 @@
 import dbConnectionFunc from "../dbconnection/dbconnection.js";
 
 async function getRole(_, res) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
   console.log("getRole (+)");
 
   let dbconnect;
--- apis/insertNewMedicineStock.js
+++ apis/insertNewMedicineStock.js
@@ -1,6 +1,11 @@
 import dbConnectionFunc from "../dbconnection/dbconnection.js";
 
 async function insertNewMedicineStock(req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
   console.log("insertNewMedicineStock (+)");
 
   let dbconnect;
